c72ba2b74adeb0fe752311b1b6a4c279cde79d76
Ruslan Petrovich <rkomehdaht@gmail.com>
2021-07-06 23:38:04 +0300

    New version with corrections

--- BashHW.sh
+++ BashHW.sh
@@ -3,7 +3,7 @@ sudo apt-get install mc
 #Creating 100 *.txt files
 touch {1..100}.txt
 #Appending "Hello World" to even-name files
-for i in [2,4,6,8,0].txt
+for i in {2..100..2}.txt
 do
     echo "Hello World" >> $i
 done
